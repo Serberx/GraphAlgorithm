396c77ce8e4e30b60f60375c0428a8e92c64bb37
Savas Kuzu <kuz14544@spengergasse.at>
2023-06-27 03:33:30 +0200

    Brücken wurden dem Grafenprogramm hinzugefügt

--- G_Programm/src/constantsSec/ConstantsHiden.java
+++ G_Programm/src/constantsSec/ConstantsHiden.java
@@ -2,7 +2,7 @@ package constantsSec;
 
 public class ConstantsHiden{
 
-    public static String PW = "kuzusavas999";
-    public static String URL = "jdbc:mysql://localhost:3308/testdb";
-    public static String USER = "root";
+    public static String PW = " ";
+    public static String URL = "jdbc:mysql://localhost: ";
+    public static String USER = " ";
 }
